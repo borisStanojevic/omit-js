7fdb2409d96d56134f95ec51020286e365265041
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-07-21 21:16:34 +0000

    Format.

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const omit = (properties, source) => {
-if(!Array.isArray(properties)) 
-  throw Error("Properties argument must be an array.");
-if(source === undefined || source === null)
-  throw Error("Source argument cannot be undefined or null.");
-if(typeof(source) !== "object")
-  throw Error("Source argument must be an object.");
+  if (!Array.isArray(properties))
+    throw Error("Properties argument must be an array.");
+  if (source === undefined || source === null)
+    throw Error("Source argument cannot be undefined or null.");
+  if (typeof(source) !== "object")
+    throw Error("Source argument must be an object.");
 
-if(properties.length === 1) {
-  const {[properties[0]]: _, ...rest} = source;
-  return rest;
-}
-  
-return Object.keys(source).filter(key => !properties.includes(key))
-  .reduce((destination, key) => ({
-    ...destination,
-    [key]: source[key]
-  }), {});
+  if (properties.length === 1) {
+    const {
+      [properties[0]]: _, ...rest
+    } = source;
+    return rest;
+  }
+
+  return Object.keys(source).filter(key => !properties.includes(key))
+    .reduce((destination, key) => ({
+      ...destination,
+      [key]: source[key]
+    }), {});
 }
 
 const x = {
