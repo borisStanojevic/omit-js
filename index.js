66dc89db69669a6027926ca6b83e3e5a7b5e5972
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-07-21 21:04:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const omit = (properties, source) => Object.keys(source).filter(key => !properties.includes(key)).reduce((destination, key) => ({
+  ...destination,
+  [key]: source[key]
+}), {});
+
+const x = {
+  a: "a",
+  b: "b",
+  c: "c",
+  d: "d",
+  e: "e"
+};
+
+const y = omit(["a", "e"], x);
+
+console.log(y);
