cf0031cf911063a3289d9b3a8fca76eeb458ac36
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2022-07-21 21:16:01 +0000

    Add validation.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const omit = (properties, source) => Object.keys(source).filter(key => !properties.includes(key))
+const omit = (properties, source) => {
+if(!Array.isArray(properties)) 
+  throw Error("Properties argument must be an array.");
+if(source === undefined || source === null)
+  throw Error("Source argument cannot be undefined or null.");
+if(typeof(source) !== "object")
+  throw Error("Source argument must be an object.");
+
+if(properties.length === 1) {
+  const {[properties[0]]: _, ...rest} = source;
+  return rest;
+}
+  
+return Object.keys(source).filter(key => !properties.includes(key))
   .reduce((destination, key) => ({
     ...destination,
     [key]: source[key]
   }), {});
+}
 
 const x = {
   a: "a",
@@ -12,6 +26,6 @@ const x = {
   e: "e"
 };
 
-const y = omit(["a", "e"], x);
+const y = omit(["a"], x);
 
 console.log(y);
